bf089d127419bd85b67aa4b149564e73230c0e2e
elishayer <eshayer@stanford.edu>
2015-12-30 23:28:25 -0900

    Initial stub website

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* main.js
+ * Eli Shayer
+ * ----------
+ * Kathryn Treder's website main Javascript file. Currently empty.
+ */
+
+(function(window, document, undefined) {
+	console.log('main.js connected');
+})(window, document);
